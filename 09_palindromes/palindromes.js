ed09b2d583659572b377786889a58d2d2d2077a3
Stefan <floydrise@gmail.com>
2024-10-01 23:48:31 +0300

    Finished palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    const cleanedStr = word.replace(/[^0-9a-z]/gi, '').toLowerCase();
+    const reverseStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === reverseStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
