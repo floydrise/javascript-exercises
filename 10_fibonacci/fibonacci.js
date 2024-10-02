a65d20613e04d66383cabd3758a79895bf32c8dc
Stefan <floydrise@gmail.com>
2024-10-02 19:12:27 +0300

    Complete the fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+function fibonacci(n) {
+    if (typeof n === "string") {
+        n = Number(n);
+    }
 
-};
+    if (n < 0) return "OOPS";
+
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
