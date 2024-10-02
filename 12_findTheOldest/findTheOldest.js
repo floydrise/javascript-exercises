1d5d250b7f3f4f308ec4d9eed21d21e624468ae5
Stefan <floydrise@gmail.com>
2024-10-02 20:38:07 +0300

    Complete last challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    var currentYear = new Date().getFullYear();
+    return people.reduce(function (oldestPerson, currentPerson) {
+        var oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
+        var personAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+        return personAge > oldestAge ? currentPerson : oldestPerson;
+    });
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
